cb5263e4ef54b916deaf347dee84761b93872a40
Catherine Powell <catherine@abakas.com>
2015-02-09 11:46:03 -0500

    wip

--- env_var/recipes/default.rb
+++ env_var/recipes/default.rb
@@ -1,12 +1,15 @@
 Chef::Log.info("sourcing environment variables initializer")
-Chef::Log.info("environment variables initializer deploy #{node[:deploy]}")
-Chef::Log.info("environment variables initializer deploy #{node[:deploy][:white_label][:deploy_to]}")
-Chef::Log.info("environment variables initializer deploy #{node[:deploy][:white_label][:environment_variables]}")
-Chef::Log.info("environment variables initializer deploy #{node[:deploy][application]}")
-Chef::Log.info("environment variables initializer deploy #{node[:deploy][application][:environment_variables]}")
-template "#{node[:deploy][:white_label][:deploy_to]}/shared/config/env_vars.rb" do
-  source "env_vars.rb.erb"
-  mode 0644
-  owner node[:deploy][:white_label][:user]
-  group node[:deploy][:white_label][:group]
+
+node[:deploy].each do |application, deploy|
+  Chef::Log.info("environment variables initializer deploy #{node[:deploy]}")
+  Chef::Log.info("environment variables initializer deploy #{node[:deploy][application][:deploy_to]}")
+  Chef::Log.info("environment variables initializer deploy #{node[:deploy][application][:environment_variables]}")
+
+  template "#{node[:deploy][application][:deploy_to]}/shared/config/env_vars.rb" do
+    source "env_vars.rb.erb"
+    mode 0644
+    owner node[:deploy][:white_label][:user]
+    group node[:deploy][:white_label][:group]
+    ignore_failure true
+  end
 end
