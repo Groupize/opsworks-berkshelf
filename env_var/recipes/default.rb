bd6bd7ecaab05155405e30ddb61b8eea0b237f85
Catherine Powell <catherine@abakas.com>
2015-02-09 11:09:31 -0500

    wip

--- env_var/recipes/default.rb
+++ env_var/recipes/default.rb
@@ -1,7 +1,9 @@
 Chef::Log.info("sourcing environment variables initializer")
-Chef::Log.info("deploy #{node[:deploy]}")
-Chef::Log.info("deploy #{node[:deploy][:white_label][:deploy_to]}")
-Chef::Log.info("deploy #{node[:deploy][:white_label][:environment_variables]}")
+Chef::Log.info("environment variables initializer deploy #{node[:deploy]}")
+Chef::Log.info("environment variables initializer deploy #{node[:deploy][:white_label][:deploy_to]}")
+Chef::Log.info("environment variables initializer deploy #{node[:deploy][:white_label][:environment_variables]}")
+Chef::Log.info("environment variables initializer deploy #{node[:deploy][application]}")
+Chef::Log.info("environment variables initializer deploy #{node[:deploy][application][:environment_variables]}")
 template "#{node[:deploy][:white_label][:deploy_to]}/shared/config/env_vars.rb" do
   source "env_vars.rb.erb"
   mode 0644
